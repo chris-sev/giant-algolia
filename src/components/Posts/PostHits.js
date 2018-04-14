1d36fde8277b4278c613398726e0a1c1062714dd
Chris Sevilleja <chris@scotch.io>
2018-04-14 11:28:32 -0700

    ads in post grid

--- src/components/Posts/PostHits.js
+++ src/components/Posts/PostHits.js
@@ -12,11 +12,20 @@ const InfiniteHits = connectInfiniteHits(({ hits, refine }) => (
   <React.Fragment>
     {/* article grid */}
     <div className="columns is-multiline">
-      {hits.map(hit =>
+
+      {/* show an ad if the index is divisible by 12 */}
+      {/* otherwise, show the card */}
+      {hits.map((hit, index) => (
         <div key={hit.id} className="column is-3">
-          <PostCard post={hit} />
+          {(index !== 0 && index % 10 === 0) ? (
+            <div className="is-flex-centered">
+              <img alt="Ad" src="https://placeimg.com/300/250/any" className="ad" />
+            </div>
+          ):(
+            <PostCard post={hit} />
+          )}
         </div>
-      )}
+      ))}
     </div>
 
     {/* read more button */}
