25491119afdaac3e4394ae6be82b9cdd4fe75dac
Chris Sevilleja <chris@scotch.io>
2018-04-14 19:34:35 -0700

    stuff

--- src/components/Cards/CardImage.js
+++ src/components/Cards/CardImage.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import './CardImage.css';
 
+/**
+ * Quick way to convert seconds to minutes
+ */
+const convertToMinutes = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = (time - minutes * 60).toString().padStart(2, '0');
+
+  return `${minutes}:${seconds}`;
+};
+
+/**
+ * The main card image for all our course/post cards
+ */
 const CardImage = ({ article, showPlayButton }) => {
+  console.log(article);
   const url         = article.published_url;
   const imageStyles = {
     backgroundImage: `url('${article.image_thumbnail || article.image}')`
@@ -12,8 +26,10 @@ const CardImage = ({ article, showPlayButton }) => {
       <div className="image is-2by1" style={imageStyles}></div>
 
       {/* the play button */}
-      {showPlayButton && (
-        <div className="play">play button</div>
+      {showPlayButton && article.duration && (
+        <div className="play">
+          {convertToMinutes(article.duration)}
+        </div>
       )}
     </a>
   )
