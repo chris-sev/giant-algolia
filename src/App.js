14b95c560555d992878fb4d9c2b42a3eb2bb7bcb
Chris Sevilleja <chris@scotch.io>
2018-04-28 14:43:51 -0700

    header work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import NavSearchBox from './components/NavSearchBox';
 
 import Home from './containers/Home';
 import Guides from './containers/Guides';
@@ -12,53 +13,53 @@ import Posts from './containers/Posts';
 import Learn from './containers/Learn';
 import Authors from './containers/Authors';
 
+import logo from './logo.svg';
+
 import 'bulma/css/bulma.css';
 import './App.css';
+import './Header.css';
 import './components/Cards/Card.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <React.Fragment>
-          <nav className="navbar is-dark is-fixed-top">
-          <div className="container is-fluid">
-            
-            {/* logo */}
-            <div className="navbar-brand">
-              <Link className="navbar-item" to="/">Scotch</Link>
-            </div>
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <nav id="scotch-nav" className="navbar is-dark">
+      <div className="container is-fluid">
+        
+        {/* logo */}
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">
+            <img className="scotch-logo" src={logo} width="80" />
+          </Link>
+        </div>
 
-            {/* menu */}
-            <div className="navbar-menu">
-              <div className="navbar-start">
-                <Link className="navbar-item" to="/">Home</Link>
-                <Link className="navbar-item" to="/guides">Guides</Link>
-                <Link className="navbar-item" to="/courses">Courses</Link>
-                <Link className="navbar-item" to="/posts">Posts</Link>
-              </div>
-              <div className="navbar-end">
-                <Link className="navbar-item" to="/learn">Learn</Link>
-                <Link className="navbar-item" to="/search">Search</Link>
-                <Link className="navbar-item" to="/authors">Authors</Link>
-              </div>
-            </div>
-
+        {/* menu */}
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <Link className="navbar-item" to="/learn">Learn</Link>
+            <Link className="navbar-item" to="/guides">Guides</Link>
+            <Link className="navbar-item" to="/courses">Courses</Link>
+            <Link className="navbar-item" to="/posts">Posts</Link>
           </div>
-          </nav>
+          <div className="navbar-end">
+            <NavSearchBox />
+            <Link className="navbar-item" to="/authors">Authors</Link>
+          </div>
+        </div>
 
-          {/* define our routes */}
-          <Route exact path="/" component={Home} />
-          <Route path="/guides" component={Guides} />
-          <Route path="/courses" component={Courses} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/learn" component={Learn} />
-          <Route path="/search" component={Learn} />
-          <Route path="/authors" component={Authors} />
-        </React.Fragment>
-      </Router>
-    );
-  }
-}
+      </div>
+      </nav>
+
+      {/* define our routes */}
+      <Route exact path="/" component={Home} />
+      <Route path="/guides" component={Guides} />
+      <Route path="/courses" component={Courses} />
+      <Route path="/posts" component={Posts} />
+      <Route path="/learn" component={Learn} />
+      <Route path="/search" component={Learn} />
+      <Route path="/authors" component={Authors} />
+    </React.Fragment>
+  </Router>
+)
 
 export default App;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,10 +5,23 @@ import { connectSearchBox } from 'react-instantsearch/connectors';
 export default connectSearchBox(({ refine }) => {
   const debouncedSearch = debounce(e => refine(e.target.value), 200);
 
+  const getParameterByName = name => {
+      const url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+  };
+
   const onChange = e => {
     e.persist();
     debouncedSearch(e, e.eventTarget);
   };
+
+  // was the search box already typed into?
+  const currentSearch = getParameterByName('q');
 
   return <input 
     placeholder="Search here..." 
@@ -20,5 +33,6 @@ export default connectSearchBox(({ refine }) => {
     required="" 
     maxLength="512" 
     className="ais-SearchBox-input"
-    onChange={onChange} />
+    onChange={onChange}
+    value={currentSearch} />
 });
